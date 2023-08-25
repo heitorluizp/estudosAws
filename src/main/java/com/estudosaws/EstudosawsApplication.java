91709bfa659f5e7b423a597754ec6c97e0b4e7e9
Heitor Luiz <heitorluiz@MacBook-Air-de-Heitor.local>
2023-08-24 22:45:16 -0300

    Commit Inicial

---
+++ src/main/java/com/estudosaws/EstudosawsApplication.java
@@ -0,0 +1,13 @@
+package com.estudosaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosawsApplication.class, args);
+	}
+
+}
